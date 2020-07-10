53c8b063761567e87d9af39aa7f428a0962a8b50
jonekeat <jonekeat@gmail.com>
2020-07-10 11:54:49 +0800

    Add files via upload

---
+++ install_packages.R
@@ -0,0 +1,46 @@
+install_packages <- function(pkgs, 
+                             repos = getOption("repos", "https://cran.rstudio.com/"),
+                             ...) {
+  # Get all loaded & attched pkgs in fresh session 
+  loaded_pkg <- loadedNamespaces()
+  
+  # Warn if non-base pkgs are loaded before installation
+  installed_pkg <- installed.packages()
+  base_pkg <- subset(as.data.frame(installed_pkg, stringsAsFactors = FALSE), 
+                     Priority == "base", select = Package)
+  base_pkg <- unique(base_pkg[[1L]])
+  non_base_loaded <- !loaded_pkg %in% base_pkg
+  if (any(non_base_loaded))
+    warning("Some non-base packages are loaded before installation: '", 
+            paste0(loaded_pkg[non_base_loaded], collapse = "', '"), 
+            "'. Installing these packages might caused installation failed", 
+            ", try run 'rm(list = ls())' & launch a new R session\n", immediate. = TRUE)
+  
+  # Redirect installation path from system lib to avoid mixing with base pkgs in Windows
+  if (.Platform$OS.type == "windows") {
+    r_version <- paste0(R.version$major, ".", substr(R.version$minor, 1, 1))
+    win_lib <- paste0(dirname(normalizePath(Sys.getenv("R_HOME"), "/")), "/win-library/", 
+                      r_version)
+    if (!dir.exists(win_lib))
+      dir.create(win_lib, recursive = TRUE)
+    .libPaths(win_lib)
+  }
+  
+  # Install binary whenever possible
+  opt <- c(options(install.packages.compile.from.source = "never"), options(warn = -1))
+  on.exit(options(opt))
+  
+  # Check if package exist
+  pkg_exist <- required_packages %in% installed_pkg[, "Package"]
+  
+  # Only install missing pkgs
+  if (any(pkg_exist))
+    message("'", paste0(required_packages[pkg_exist], collapse = "', '"),
+            "' already installed and will be skipped. ", 
+            "If you want to re-install these packages, ", 
+            "try remove it first and re-run again\n")
+  pkg_to_install <- required_packages[!pkg_exist]
+  if (length(pkg_to_install))
+    install.packages(pkg_to_install, repos = repos, ...)
+  return(invisible(NULL))
+}
